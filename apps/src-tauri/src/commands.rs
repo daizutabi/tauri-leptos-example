7a2bf2a5f9b26c182ba13c3a1e5e21d5f13222bb
Daizu <daizutabi@gmail.com>
2024-08-21 05:21:41 +0900

    add test

--- apps/src-tauri/src/commands.rs
+++ apps/src-tauri/src/commands.rs
@@ -3,3 +3,14 @@ pub fn greet(name: String) -> String {
     println!("invoked: {name}");
     format!("Hello, {name}!")
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn hello() {
+        let result = greet("Rust".to_string());
+        assert_eq!(result, "Hello, Rust!");
+    }
+}
